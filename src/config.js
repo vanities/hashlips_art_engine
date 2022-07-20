ef410aa2075e9ef51703662e0499b9162b271f67
Arshad Wala <asamplewala@gmail.com>
2022-07-20 08:53:59 -0700

    remove example layers

--- src/config.js
+++ src/config.js
@@ -16,13 +16,7 @@ const layerConfigurations = [
   {
     growEditionSizeTo: 5,
     layersOrder: [
-      { name: "Background" },
-      { name: "Eyeball" },
-      { name: "Eye color" },
-      { name: "Iris" },
-      { name: "Shine" },
-      { name: "Bottom lid" },
-      { name: "Top lid" },
+      // { name: "" },
     ],
   },
 ];
